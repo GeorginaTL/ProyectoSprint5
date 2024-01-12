a0fc41eb3b412aa4a2189740d28758a7974b52cb
Georgina <georginatelloleon@gmail.com>
2024-01-11 21:30:51 -0700

    Comentarios sobre los gráficos

--- app.py
+++ app.py
@@ -13,11 +13,13 @@ if hist_button:
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
     fig = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
-    #fig.show() # dibujar histograma
+    st.write('La mayoría de los autos a la venta han recorrido 1000 Millas, segun el odómetro')
+
 
 scatter_button = st.button('Construir gráfico de dispersión') # crear un botón
 if scatter_button:
     st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
     fig = px.scatter(car_data, x="odometer", y="price")
     st.plotly_chart(fig, use_container_width=True)
-    #fig.show() # dibujar gráfico de dispersión 
+    st.write('Los autos que se venden a precios mas altos son los mas nuevos (con odómetro 0). A partir de 2000 Millas, su precio empieza a bajar')
+  
