2c9d8834ceceb5b61fcfaaa1be10f87c20ca1ff9
Georgina <georginatelloleon@gmail.com>
2024-01-11 21:58:13 -0700

    agregue dos casillas

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ProyectoSprint5
-Proyecto en construcción.
+El presente es un breve estudio sobre las características de los autos a la venta. Para consideración de los posibles compradores.
+Algunos tienen características más frecuentes. Como su marca, Millas recorridas, o como puede cambiar su precio de acuerdo a las características. Por ejemplo: su edad o Millas recorridas pueden decidir el precio del auto.
+El histograma describe cuantas millas han recorrido la mayoría de ellos, mientras que el gráfico de dispersión permite visualizar cómo cambia su precio de acuerdo a las millas recorridas por los autos.
+Seleccione una casilla de verificación: 
+- una de ellas le mostrará en un histograma cuantos autos aproximadamente tienen cierto valor en su odómetro.
+- la otra le permitirá saber el precio aproximado de acuerdo a las Millas que ha recorrido el auto en su odómetro.
+
+Adelante, puede dar click en el presente enlace, si esta información no es de utilidad no dude en contactarme, dejaré mi correo electrónico mas abajo:
+https://analisis-estadistico-sobre-autos-a-la.onrender.com
+
+georginatelloleon@gmail.com
--- app.py
+++ app.py
@@ -7,19 +7,36 @@ st.header("Análisis exploratorio de datos sobre autos")
 
 st.write("\n",car_data.sample(10),"\n")
 
+# casilla de verificación para el histograma
+build_histogram = st.checkbox('Construir un histograma')
 
-hist_button = st.button('Construir histograma') # crear un botón
-if hist_button:
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma: frecuencia para los valores de la columna odómetro')
     fig = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
     st.write('La mayoría de los autos a la venta han recorrido 1000 Millas, segun el odómetro')
 
+    # casilla de verificación para el gráfico de dispersión
+build_scatter = st.checkbox('Construir un gráfico de dispersión')
 
-scatter_button = st.button('Construir gráfico de dispersión') # crear un botón
-if scatter_button:
-    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+if build_scatter: # si la casilla de verificación está seleccionada
+    st.write('Construir un gráfico de dispersión: odómetro vs precio del auto')
     fig = px.scatter(car_data, x="odometer", y="price")
     st.plotly_chart(fig, use_container_width=True)
     st.write('Los autos que se venden a precios mas altos son los mas nuevos (con odómetro 0). A partir de 2000 Millas, su precio empieza a bajar')
+
+#hist_button = st.button('Construir histograma') # crear un botón
+#if hist_button:
+#    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+#    fig = px.histogram(car_data, x="odometer")
+#    st.plotly_chart(fig, use_container_width=True)
+#    st.write('La mayoría de los autos a la venta han recorrido 1000 Millas, segun el odómetro')
+
+
+#scatter_button = st.button('Construir gráfico de dispersión') # crear un botón
+#if scatter_button:
+#    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+#    fig = px.scatter(car_data, x="odometer", y="price")
+#    st.plotly_chart(fig, use_container_width=True)
+#    st.write('Los autos que se venden a precios mas altos son los mas nuevos (con odómetro 0). A partir de 2000 Millas, su precio empieza a bajar')
   
