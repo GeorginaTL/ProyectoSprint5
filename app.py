2e994a0d3789604c650126d94caaeed88068a130
Georgina <georginatelloleon@gmail.com>
2024-01-11 21:15:47 -0700

    agregue tabla

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ import streamlit as st
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 
 st.header('Análisis exploratorio de datos sobre autos')
-
+print("\n",car_data.sample(10),"\n")
 hist_button = st.button('Construir histograma') # crear un botón
 if hist_button:
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
     fig = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
     #fig.show() # dibujar histograma
+
 scatter_button = st.button('Construir gráfico de dispersión') # crear un botón
 if scatter_button:
     st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
--- notebooks/app.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import plotly.express as px
-import streamlit as st
-car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-
-st.header('Análisis exploratorio de datos sobre autos')
